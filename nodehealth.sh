5ff262f933dfb00b026a1abef7d4c7dcad7b8df4
keerthi-44 <71441727+keerthi-44@users.noreply.github.com>
2025-01-08 20:14:14 +0530

    Add files via upload

---
+++ nodehealth.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+######################
+#Author: Keerthi
+#Date: 08/12/2025
+#
+#This script outputs the node health
+#
+#Version: v1
+######################
+
+set -x #debug mode
+df -h 
+
+free -g
+
+nproc
